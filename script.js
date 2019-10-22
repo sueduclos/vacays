362917290dbb534dfda618645caacbf45c50c620
Sue Duclos <sue.duclos@gmail.com>
2019-10-21 21:32:52 -0700

    added function

--- script.js
+++ script.js
@@ -22,3 +22,58 @@ if (wantsToBook === 'yes') {
 } else {
     document.write('Please come back when you are ready to getaway!');
 }
+
+// vacation html pseudocode
+
+// inputs (parameters): name, image, description
+// work: insert the name, image, and description into the tags
+// outputs (return values): an html article with name, image, description.
+
+//test case
+//parameters: 'Jacque', 'jacques.jpg', 'Not the brightest but really sweet'
+
+//work value:
+//<article>
+ // <h3>name</h3>
+ // <img src="image">   
+ // <p>description</p>
+ //<article>
+
+ //return value:
+//<article>
+ // <h3>Jacques</h3>
+ // <img src="jacques.jpg">   
+ // <p>Not the brightest but really sweet</p>
+ //<article>
+
+//  function createCatArticle(name, image, description) {
+//      var catArticle = '<article>' + 
+//         '<h3>' + name + '</h3>' +
+//         '<img src="image">' + image + '">' +
+//         '<p>' + description + </p> +
+//         ''</article>';
+    
+//     return catArticle;
+//  }
+
+function createVacationDestination(name, image, description) {
+    var vacationDestination = '<article>' + 
+        '<h3>' + name + '</h3>' +
+        '<img src="' + image + '">' +
+        '<p>' + description + '</p>' +
+        '</article>';
+
+    return vacationDestination;
+}
+
+var wantsToBook = prompt('Hey there! Do you want to add a desination?');
+
+if (wantsToBook === 'yes') {
+    var destinationName = prompt('Where do you want to go?');
+    var destinationImage = prompt('What is the name of the file containing the image?');
+    var destinationDescription = prompt('What is the description of the new destination?');
+    var newDestination = createVacationDestination(destinationName, destinationImage, destinationDescription);
+    document.write(newDestination);
+}
+
+
